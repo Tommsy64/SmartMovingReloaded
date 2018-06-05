a40bc19e8c8de9ee7a7a83a0c1da404ee32f7b68
Thomas Pakh <git@tommsy.com>
2018-06-05 15:46:38 -0400

    Make license header prettier

--- src/main/java/com/tommsy/playerapi/core/MixinPlayerAPIPlugin.java
+++ src/main/java/com/tommsy/playerapi/core/MixinPlayerAPIPlugin.java
@@ -1,20 +1,21 @@
-// ==========================================================================
-// Mixin Player API
-// Copyright (C) 2018  Tommsy64
-//
-// Mixin Player API is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Mixin Player API is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Mixin Player API.  If not, see <http://www.gnu.org/licenses/>.
-// ==========================================================================
+/*
+* Mixin Player API
+* Copyright (C) 2018  Tommsy64
+*
+* Mixin Player API is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Mixin Player API is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Mixin Player API.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.tommsy.playerapi.core;
 
 import java.util.Map;
